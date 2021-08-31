f6d5910e9028208d91c7980c2e9315949be080e1
ipitio <pekogan@gmail.com>
2021-08-31 14:20:38 -0400

    hw1q2

--- hw/1/1.r
+++ hw/1/1.r
@@ -1,29 +1,37 @@
-# set this file's dir as working dir
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse)
 library(tidyverse)
 this_dir <-  function() {
-    this_file <- commandArgs() %>%
-    tibble::enframe(name = NULL) %>%
-    tidyr::separate(col = value, into = c("key", "value"),
-                    sep = "=", fill = "right") %>%
-    dplyr::filter(key == "--file") %>%
-    dplyr::pull(value)
-    if (length(this_file) == 0) {
-      this_file <- rstudioapi::getSourceEditorContext()$path
-    }
-    return(dirname(this_file))
+    this_dir <- commandArgs() %>%
+      tibble::enframe(name = NULL) %>%
+      tidyr::separate(fill = "right", col = value, sep = "=",
+                      into = c("key", "value")) %>%
+      dplyr::filter(key == "--file") %>%
+      dplyr::pull(value)
+    if (length(this_dir) == 0)
+      this_dir <- rstudioapi::getSourceEditorContext()$path
+    return(dirname(this_dir))
 }
-setwd(this_dir())
+setwd(this_dir()) # output will be saved where this file is located
+output <- "1.txt" # name of output file
+
+# For best results, set tab to 4 spaces #
 
 # Q1
 
 height <- c(1.55, 1.92, 1.60, 1.75, 1.58, 1.67,
             1.63, 1.82, 1.76, 1.77, 1.72, 1.85)
-cat("Q1:\n\tmean:\t\t", mean(height), "\n\tsd:\t\t\t", sd(height),
-    "\n\tlen:\t\t", length(height), "\n\tnum < 1.65:\t", sum(height < 1.65),
-    "\n\tis 1.6 < val < 1.75:\n\t\t\t\t", file = "1.txt")
-write.table(data.frame(height, c(height > 1.6 & height < 1.75)), "1.txt",
-            TRUE, sep = " \t", row.names = FALSE, eol = "\n\t\t\t\t",
+cat("# For best results, set tab to 4 spaces #\n\nQ1:\t1)\t",
+    mean(height), "\n\t2)\t", sd(height), "\n\t3)\t", length(height),
+    "\n\t4)\t", sum(height < 1.65), "\n\t5)\t ", file = output)
+write.table(data.frame(height, c(height > 1.6 & height < 1.75)), output,
+            TRUE, sep = " \t", row.names = FALSE, eol = "\n\t\t ",
             col.names = FALSE)
-cat("\n", file = "1.txt", append = TRUE)
+cat("\n", file = output, append = TRUE)
+
+# Q2
+
+tmp <- matrix(rnorm(12), 3, 4)
+cat("Q2:\t1)\t", sum(rowSums(tmp)[-1]), "\n\t2)\t",
+    prod(colSums(tmp)[c(2, 4)]), "\n\t3)\t", dim(tmp), "\n\t4)\t",
+    c((less <- tmp[2, ])[less < 0.2]), "\n\n", file = output, append = TRUE)
