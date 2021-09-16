721cb7ad8137fbe40bd1461b6361a0165a078cd7
ipitio <pekogan@gmail.com>
2021-09-16 14:40:32 -0400

    hw2 + hw1 pdfs

---
+++ hw/1/1.r
@@ -0,0 +1,16 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse)
+library(tidyverse)
+this_dir <-  function() {
+    this_dir <- commandArgs() %>%
+      tibble::enframe(name = NULL) %>%
+      tidyr::separate(fill = "right", col = value, sep = "=",
+                      into = c("key", "value")) %>%
+      dplyr::filter(key == "--file") %>%
+      dplyr::pull(value)
+    if (length(this_dir) == 0)
+      this_dir <- rstudioapi::getSourceEditorContext()$path
+    return(dirname(this_dir))
+}
+setwd(this_dir())
+rmarkdown::render(paste("Paul Kogan-HW", 1, ".rmd", sep = ""))
