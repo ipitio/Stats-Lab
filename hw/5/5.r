00c9aed7537cda9990fe2e0c5b9236958d4a5bb1
ipitio <21136719+ipitio@users.noreply.github.com>
2021-10-10 02:19:34 -0400

    hw5init

---
+++ hw/5/5.r
@@ -0,0 +1,32 @@
+options(warn = -1)
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ISwR, MASS)
+library(tidyverse)
+this_dir <-  function() {
+    this_dir <- commandArgs() %>%
+      tibble::enframe(name = NULL) %>%
+      tidyr::separate(fill = "right", col = value, sep = "=",
+                      into = c("key", "value")) %>%
+      dplyr::filter(key == "--file") %>%
+      dplyr::pull(value)
+    if (length(this_dir) == 0)
+      this_dir <- rstudioapi::getSourceEditorContext()$path
+    return(dirname(this_dir))
+}
+setwd(this_dir())
+
+rjct <- function(lvl) paste("<= alpha =", lvl,
+    "so reject the null\n\t\t\thypothesis that the")
+conc <- function(test, hyp, lvl = 1 - attr(test$conf.int, "conf.level"),
+                 f = T) {
+    lvl <- ifelse(length(lvl) == 0, 0.05, lvl)
+    val <- test$p.value
+    str <- paste("conclusion: p-value =", format(round(val, 4), nsmall = 4),
+           ifelse(val <= lvl, rjct(lvl), rjct(lvl) %>%
+           str_replace_all(c("<=" = ">", "so" = "so fail to"))), hyp)
+    if (f) cat(str)
+    invisible(str)
+}
+
+# Q1
+
